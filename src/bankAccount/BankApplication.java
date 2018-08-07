6d0bece252f038c586765292b9ce506f8a8ca9c5
guinn8 <41174494+guinn8@users.noreply.github.com>
2018-08-07 13:36:17 -0600

    fIX

--- src/bankAccount/BankApplication.java
+++ src/bankAccount/BankApplication.java
@@ -1,6 +1,10 @@
 package bankAccount;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
 import java.io.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -12,6 +16,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 /**
  * <h1>BankApplication<h1>
@@ -26,23 +31,12 @@ import javafx.scene.control.ToggleGroup;
  * @version 1.0
  */
 public class BankApplication extends Application {
-
+	String accountType;
+	
 	BufferedReader reader = null;
 	BankAccount account;
 	Customer customer;
-	
-	/** pre-set customer savings account */
-	SavingsAccount savings= new SavingsAccount(150.00,0.00);
-
-
-	
-	/**
-	 * blank constructor 
-	 * assigns the customer to the SavingsAccount 
-	 */
-	public BankApplication(){
-		savings.setCustomer(customer);
-	}
+	String custname;
 	
 	
 	public static void main(String[] args){
@@ -59,7 +53,7 @@ public class BankApplication extends Application {
 		int counter = 0;
 		
 		try {
-			File accountFile=new File("accountFiles/account1");
+			File accountFile=new File("accountFiles/account");
 			reader=new BufferedReader(new FileReader(accountFile));
 			
 			String line =reader.readLine();
@@ -75,12 +69,14 @@ public class BankApplication extends Application {
 			
 			Button executeButton= new Button("Execute");
 			customer= new Customer(accountArr[0],Integer.parseInt(accountArr[2]));
-			if (accountArr[1].equals("SavingsAccount")) account = new SavingsAccount(customer,Double.parseDouble(accountArr[2]));
-			else if (accountArr[1].equals("ChequingAccount")) account = new ChequingAccount(customer, Double.parseDouble(accountArr[2]), Double.parseDouble(accountArr[5]));
+			
+			
+			if (accountArr[1].equals("SavingsAccount")) account = new SavingsAccount(customer,Double.parseDouble(accountArr[3]));
+			else if (accountArr[1].equals("ChequingAccount")) account = new ChequingAccount(customer, Double.parseDouble(accountArr[3]), Double.parseDouble(accountArr[4]));
 			
 			Label customerNameLabel= new Label("Name: " +customer.getName());
 			Label customerIDLabel = new Label("Account ID: " + Integer.toString(customer.getID()));
-			Label balanceLabel = new Label("Balance: " +Double.toString(savings.getBalance()));
+			Label balanceLabel = new Label("Balance: " +Double.toString(account.getBalance()));
 			
 			depositTextField.setPromptText("Enter Deposit amount: ");
 			depositTextField.setPrefColumnCount(15);
@@ -116,9 +112,9 @@ public class BankApplication extends Application {
 			    	// tries to parse data in deposit field
 			    	try{ 
 			    		double deposit=Double.parseDouble(depositTextField.getText());
-			    		savings.deposit(deposit);
+			    		account.deposit(deposit);
 			    		depositTextField.clear();
-			    		balanceLabel.setText("Balance: " +Double.toString(savings.getBalance()));
+			    		balanceLabel.setText("Balance: " +Double.toString(account.getBalance()));
 			    		isBadInput=false; // if the parse doesn't fail input is good
 			    		
 			    	   }catch(Exception badInputDeposit){
@@ -128,9 +124,9 @@ public class BankApplication extends Application {
 			    	// tries to parse data in withdraw field	
 			    	try{
 			    		double withdraw=Double.parseDouble(withdrawTextField.getText());
-			    		savings.withdraw(withdraw);
+			    		account.withdraw(withdraw);
 			    		withdrawTextField.clear();
-			    		balanceLabel.setText("Balance: " +Double.toString(savings.getBalance()));
+			    		balanceLabel.setText("Balance: " +Double.toString(account.getBalance()));
 			    		isBadInput=false; // if the parse doesn't fail input is good
 			    		
 			    		}catch(Exception badInputWithdraw){
@@ -146,31 +142,100 @@ public class BankApplication extends Application {
 		
 			
 		}catch(IOException e) {
-			File account=new File("accountFiles/account1");
-			PrintWriter writer = new PrintWriter(account);
-			writer.println("hello");
-			writer.close();
+			
 			
 			VBox vbox=new VBox(3);
 			HBox radioBox= new HBox(2);
 			
-			 ToggleGroup tGroup = new ToggleGroup();
-			
+			ToggleGroup tGroup = new ToggleGroup();
 			RadioButton chequing =new RadioButton("Chequing Account");
 			RadioButton saving =new RadioButton("Savings Account");
-			
-			vbox.getChildren().add(radioBox);
-			
 			chequing.setToggleGroup(tGroup);
 			saving.setToggleGroup(tGroup);
+			chequing.setUserData("ChequingAccount");
+			saving.setUserData("SavingsAccount");
 			
+			vbox.getChildren().add(radioBox);
 			radioBox.getChildren().add(chequing);
 			radioBox.getChildren().add(saving);
+			
+			TextField name = new TextField();
+			name.setPromptText("Enter your name");
+			int idNum=(int)(Math.random()*((9999-2000)+1))+2000;
+			Label id= new Label("Customer ID: "+Integer.toString(idNum));
+			
+			Button execute= new Button("execute");
+			
+			execute.setOnAction(new EventHandler<ActionEvent>() { //creates a new event handler object
+				
+				/**
+				 * this method processes the data in the text fields and ensures it is a double
+				 * @param e is a ActionEvent object
+				 */
+			    public void handle(ActionEvent e) {
+			    
+			    	
+			    	
+			    	// tries to parse data in deposit field
+			    	if(name.getText()!=null) {
+			    	
+			    		 custname =name.getText();
+			    		
+						
+			    		 Platform.exit();
+			    		
+			    	}
+			    	
+			    	File account=new File("accountFiles/account");
+					PrintWriter writer;
+					try {
+						writer = new PrintWriter(account);
+						writer.println(custname);
+						writer.println(accountType);
+						writer.println(idNum);
+						writer.println(0.00);
+						writer.print(0.00);
+						writer.close();
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+			    	
+			    	
+			    }
+			});
+			
+			vbox.getChildren().add(name);
+			vbox.getChildren().add(id);
+			vbox.getChildren().add(execute);
+			
+			
+			
+			tGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+
+				@Override
+				public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+					if (tGroup.getSelectedToggle()!= null) {
+						accountType=(String) tGroup.getSelectedToggle().getUserData();
+						
+					}
+					
+				}
+				
+			});
+		
+			
+			
 			
 			Scene scene = new Scene(vbox,400,200);
 			primaryStage.setTitle("BankApplication");
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			
+			
+			
+			
 			
 		}
 		
